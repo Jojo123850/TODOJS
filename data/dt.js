d8d3cb7606b5e8540df6e228e422e33ca1d801d3
Jojo123850 <giovanieandrianirina@gmail.com>
2026-06-16 16:54:27 +0200

    fix:todoNO

---
+++ data/dt.js
@@ -0,0 +1,21 @@
+
+let taches =[
+{
+	"id": 1,
+      "libelle": "Faire les courses",
+      "accomplie": true,
+      "difficulte": "moyen"
+},
+{
+	"id": 2,
+      "libelle": "Faire dodo",
+      "accomplie": false,
+      "difficulte": "facile"
+},
+{
+	"id": 3,
+      "libelle": "Faire le menage",
+      "accomplie": false,
+      "difficulte": "dificile"
+}
+]
