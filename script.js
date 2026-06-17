d387c71fff76935979c8f3d735fc4fec1ab1aa66
Jojo123850 <giovanieandrianirina@gmail.com>
2026-06-17 12:22:00 +0200

    fix:pasfini

--- script.js
+++ script.js
@@ -1,33 +1,61 @@
 
-const addButton = document.querySelector('#addButton');
-const taskInput = document.getElementById('taskInput')
-const taskList = document.getElementById('taskList')
+function ajouterLaTacheGlb(){
+    const libelleInput = document.getElementById('libelle').value;
+    const difficulteSelect  = document.getElementById('difficulte').value;
+
+    ajouterTache(libelleInput, difficulteSelect);
 
 
+    document.getElementById('libelle').value= '';
 
-addButton.addEventListener('click',() => {
-    const text = taskInput.value
-    if( text){
-        const li = document.createElement('li')
-        li.textContent = text + "";
+}
+// tache
+const accomplissement = {
+    "xp" : 0,
+    "or": 0,
+    "hp":95,
+}
+const taches = [];
 
-        const deleteButton = document.createElement('button')
-        deleteButton.textContent ="Supprimer";
+function ajouterTache(libelle,difficulte){
+    taches.push({
+        id :Date.now(),
+        accomplie: false,
+        libelle,
+        difficulte
+    })
+    
+    afficherTache(taches)
+}
 
-        deleteButton.addEventListener('click', () => {
-            li.remove();
-         });
-        li.appendChild(deleteButton)
+function afficherTache(taches){
 
-        taskList.appendChild(li);
-
-
-        taskInput.value = "";
-       
+    document.getElementById("resultat").innerHTML = taches.map(tache =>`
+    <div class="lestaches">  
+             <h3 id="tache">Nom de la tache:${tache.libelle}</h3>
+            <p id="difficulte">Difficulté:${tache.difficulte}</p>
+          
+    </div>`);    
     }
 
-});
 
+// accomplisTache
+
+function accomplisTache(index){
+
+    if(difficulte === facile){
+        xp = 10;
+        or = 5;
+    } else if(difficulte === moyen){
+        xp = 20;
+        or = 10;
+    } else{
+        xp = 30;
+        or = 15;
+    }
+}
+
+// supprimerTache
 
 
 
